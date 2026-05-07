fdcd05612fcd75c094c3f78590699909655871f0
kaushik1719 <kaushikkanani1719@gmail.com>
2026-05-08 01:07:46 +0530

    updated next

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval'",
-          },
-        ],
-      },
-    ]
-  },
+  /* config options here */
 };
 
 export default nextConfig;
