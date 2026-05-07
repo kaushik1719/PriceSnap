e039183e0c1602db5d90839198596b67fe38897c
kaushik1719 <kaushikkanani1719@gmail.com>
2026-05-08 00:51:19 +0530

    Fix CSP eval error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval'",
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
